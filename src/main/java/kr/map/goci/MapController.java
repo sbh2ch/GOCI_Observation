d8011ff92a2338c5b9bf01dca06b8b5dda5b12f3
kiost-PC\kiost <sbh2ch@gmail.com>
2017-07-12 17:07:15 +0900

    handle click event

--- src/main/java/kr/map/goci/MapController.java
+++ src/main/java/kr/map/goci/MapController.java
@@ -1,10 +1,16 @@
 package kr.map.goci;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.Scanner;
 
 /**
  * Created by kiost on 2017-07-09.
@@ -12,8 +18,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class MapController {
-    @GetMapping("/home")
-    public ResponseEntity home() {
-        return new ResponseEntity<>("Hello Spring", HttpStatus.OK);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/api/{date}/{pos}/{zoom}/{type}")
+    public ResponseEntity getValue(@PathVariable String date, @PathVariable String pos, @PathVariable String zoom, @PathVariable String type) throws Exception {
+        Scanner scan = new Scanner(new File("C:/mat/output/" + zoom + "/" + pos + ".db"));
+        String txt = "";
+
+        int x = 0;
+        int y = 0;
+        String[][] arr = new String[100][100];
+
+        while (scan.hasNext()) {
+            txt = scan.nextLine();
+            if (txt.equals("NaN"))
+                txt = "";
+
+            arr[x][y++] = txt;
+
+            if (y == 100) {
+                x++;
+                y = 0;
+            }
+        }
+        return new ResponseEntity<>(objectMapper.writeValueAsString(arr), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/lonlat/{pos}/{zoom}")
+    public ResponseEntity getLatLon(@PathVariable String pos, @PathVariable String zoom) throws Exception {
+        String[] path = {"C:/mat/output/lon" + zoom + "/" + pos + ".db", "C:/mat/output/lat" + zoom + "/" + pos + ".db"};
+        String[][][] arr = new String[2][100][100];
+
+        for (int i = 0; i < 2; i++) {
+            Scanner scan = new Scanner(new File(path[i]));
+            String txt = "";
+            int x = 0;
+            int y = 0;
+            while (scan.hasNext()) {
+                txt = scan.nextLine();
+
+                arr[i][x][y++] = txt;
+                if (y == 100) {
+                    x++;
+                    y = 0;
+                }
+            }
+        }
+        return new ResponseEntity<>(objectMapper.writeValueAsString(arr), HttpStatus.OK);
     }
 }
